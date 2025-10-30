d2088847a7de5c7cda90318d020801156e9467d5
bryanp24 <bryanp24@att.net>
2025-10-30 15:14:55 -0400

    Changed debug for input arrivals

--- src/game/net.rs
+++ src/game/net.rs
@@ -372,7 +372,7 @@ impl UdpStream {
             let buttons = ButtonFlag::from_bits_retain(chunk[5]);
 
             if cfg!(feature = "debug") {
-                println!("recieved: {dir:?}, {buttons:?} at {input_frame}");
+                println!("recieved: {dir:?}, {buttons:?} for frame: {input_frame} at local frame: {frame_at_start}");
             }
 
             let relative_frame = current_frame as isize - input_frame as isize;
